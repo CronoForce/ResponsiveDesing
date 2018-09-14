97f1a5b04fd5e548a4d576303ad4ff13c2a11585
HugoPro1 <hugo.karreno@gmail.com>
2018-09-14 11:59:15 -0500

    casi terminada

--- js/index.js
+++ js/index.js
@@ -5,10 +5,47 @@ window.addEventListener('scroll', () =>{
 })
 AOS.init();
 
-$('.light-box').hide();
-$('#open').click(function(){
-    $('.light-box').show();
+$('.light-box1').hide();
+$('#open1').click(function(){
+    $('.light-box1').show();
 })
-$('#close').click(function(){
-    $('.light-box').hide();
+$('#close1').click(function(){
+    $('.light-box1').hide();
 })
+$('.light-box2').hide();
+$('#open2').click(function(){
+    $('.light-box2').show();
+})
+$('#close2').click(function(){
+    $('.light-box2').hide();
+})
+$('.light-box3').hide();
+$('#open3').click(function(){
+    $('.light-box3').show();
+})
+$('#close3').click(function(){
+    $('.light-box3').hide();
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel')
+  
+   
+    var arrowright = document.querySelector('.arrowright')
+    var arrowleft = document.querySelector('.arrowleft')
+    var instance = M.Carousel.init(elems, {
+      fullWidth: true,
+      indicators: true
+    })
+    
+    arrowright.addEventListener('click', () => {
+      var instances = M.Carousel.getInstance(elems[0])
+      instances.next()
+    })
+    arrowleft.addEventListener('click', () => {
+      var instances = M.Carousel.getInstance(elems[0])
+      instances.prev()
+    })
+   
+    
+  })
